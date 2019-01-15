087a03fb460ba9ae105a4fbf3bcd869ceeb82819
Anders Wallenquist <anders.wallenquist@vertel.se>
2019-01-15 08:11:37 +0100

    account_invoice_follower: use partner_id instead

--- account_invoice_follower/account_invoice.py
+++ account_invoice_follower/account_invoice.py
@@ -63,7 +63,7 @@ class mail_compose_message(models.Model):
         if context.get('default_model') == 'account.invoice' and \
                 context.get('default_res_id') and context.get('mark_invoice_as_sent'):
             invoice = self.env['account.invoice'].browse(context['default_res_id'])
-            if invoice.address_invoice_id.do_not_follow:
+            if invoice and invoice.partner_id.do_not_follow:
                 invoice = invoice.with_context(mail_post_autofollow=False)
             else:
                 invoice = invoice.with_context(mail_post_autofollow=True)
