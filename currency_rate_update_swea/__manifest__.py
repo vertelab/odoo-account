f150b8e3941b08961236d89e68888aed8e921e0b
Ulf Öhrling <ulf.ohrling@vertel.se>
2019-05-23 15:07:15 +0200

    currency_rate_update_swea: fixed missing comma in manifest

--- currency_rate_update_swea/__manifest__.py
+++ currency_rate_update_swea/__manifest__.py
@@ -28,7 +28,7 @@
     'website': 'http://www.vertel.se',
     'images' : [],
     'depends' : ['currency_rate_update'],
-    'external_dependencies': {'python': ['zeep',]}
+    'external_dependencies': {'python': ['zeep',]},
     'data': [],
     'demo': [],
     'qweb': [],
