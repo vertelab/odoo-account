43c418acd8c3cba69222684f9c0dd2a5383950fb
Jakob Krabbe <jakob@syscare.se>
2019-08-01 08:15:14 +0200

    account_invoice_qr: creating project, adding code to invoice footer.

---
+++ account_invoice_qr/__manifest__.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Invoice QR-Footer',
+    'version': '1.0',
+    'summary': '',
+    'description': """
+Redesign of invoice footer using QR-code and extended contact information.
+    """,
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'demo': [],
+    'data': ['report_invoice.xml', 'invoice_data.xml'],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
