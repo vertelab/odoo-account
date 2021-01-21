0dbc7dcb90bac8382b85e857a48e5088955754aa
Nils <nils.nyman-waara@vertel.se>
2021-01-21 09:56:58 +0000

    Work in progress:
    * Upload of xml works
    * Needs to fix actual sending of invoice

--- account_inexchange/models/account_invoice.py
+++ account_inexchange/models/account_invoice.py
@@ -16,25 +16,26 @@ _logger = logging.getLogger(__name__)
 
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
-    
-    def upload_invoice(self):
+
+    def upload_invoice(self, xml_string):
         url = "https://testapi.inexchange.se/v1/api/documents"
-        # ~ client_token = self.env['res.config.settings'].inexchange_request_client_token()
         for invoice in self:
-            r = self.env['res.config.settings'].inexchange_invoice_upload('POST', url)
-            _logger.warn('HazeR %s' %r)
+            r = self.env['res.config.settings'].inexchange_invoice_upload(
+              url, xml_string)
+            return r
             # ~ r = json.loads(js.decode(r))
-            response = requests.get(url)
+            # response = requests.get(url)
             # ~ data= response.json()
             # ~ location = data['Location']
             # ~ _logger.warn('Hazedata %s'%response)
             # ~ invoice.name = r.location
-            #It has a location to send back but I can not get it out...
-            _logger.warn('Haze1+ %s' %invoice.name)
-                
+            # It has a location to send back but I can not get it out...
+
     def send_uploaded_invoice(self):
-        #all infomation are in the log, but because of other problems, it can not be upload to inexchange, I have 
-        # to fix boundary first and then get the location string back then this step will be processed.
+        # all infomation are in the log, but because of other problems,
+        # it can not be upload to inexchange, I have to fix boundary
+        # first and then get the location string back then this step
+        # will be processed.
         url = "https://testapi.inexchange.se/v1/api/documents/outbound"
         for invoice in self:
             data = {}
@@ -90,28 +91,26 @@ class account_invoice(models.Model):
                   }
                 })
             # ~ r = json.loads(r)
-            
             _logger.warn('Haze3 %s' %invoice.name)
             _logger.warn('Haze2 %s' %r)
-    
+
     def invoice_status(self):
         for invoice in self:
             url = "%s" % invoice.name
             r = self.env['res.config.settings'].inexchange_request_token('GET', url)
             r = json.loads(r)
             _logger.debug('Haze4 %s' %r)
-            
-            
-            
+
+
     def fetch_invoice(self):
         self.env['res.config.settings'].inexchange_request_client_token()
         url = "https://testapi.inexchange.se/v1/api/documents/incoming?type=invoice"
         for invoice in self:
-            
+
             r = self.env['res.config.settings'].inexchange_request_token('GET', url)
             r = json.loads(r)
             invoice.name = r['documents']['id']
-            
+
     def download_invoice(self):
         for invoice in self:
             if invoice.name:
@@ -125,31 +124,24 @@ class account_invoice(models.Model):
                 url = "https://testapi.inexchange.se/api/documents/handled" 
                 r = self.env['res.config.settings'].inexchange_request_token('POST',url,
                     data={
-                    "documents":[invoice.name]
+                    "documents": [invoice.name]
                     })
             r = json.loads(r)
-                
-            
+
+
 class AccountInvoiceSend(models.TransientModel):
     _inherit = 'account.invoice.send'
 
-    is_inexchange = fields.Boolean(string='InExchange',default=True)
-    
-    
+    is_inexchange = fields.Boolean(string='InExchange', default=True)
+
     @api.multi
     def send_and_print_action(self):
         res = super(AccountInvoiceSend, self).send_and_print_action()
         if self.is_inexchange:
             for invoice in self.invoice_ids:
-                # ~ time.sleep(1)
-                # ~ if not invoice.name:
-                # ~ raise Warning(invoice)
-                invoice.upload_invoice()
-                invoice.send_uploaded_invoice()
+                xml_string = invoice.generate_ubl_xml_string()
+                result = invoice.upload_invoice(xml_string)
+                raise Warning(f'{result.status_code}\n {result.text}')
+                invoice.send_uploaded_invoice(result)
                 invoice.invoice_status()
-        return res            
-            
-            
-    
-    
-        
+        return res
--- account_inexchange/models/res_config_settings.py
+++ account_inexchange/models/res_config_settings.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 import binascii
 import os
 import time
@@ -34,17 +34,24 @@ from odoo.http import content_disposition, dispatch_rpc, request, \
     serialize_exception as _serialize_exception, Response
 _logger = logging.getLogger(__name__)
 
+
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
-    
-    attachment_ids = fields.Many2many('ir.attachment', 'class_id', 'attachment_id', 'Attachments')
-    inexchange_apikey = fields.Char(string='API Key',config_parameter='inexchange.apikey',help="You get this code from your inexchange Account when tou activate Odoo",store=True)
-    inexchange_client_token = fields.Char(string='Client Token',config_parameter='inexchange.client.token',help="You get this code from your Odoo representative",store=True)
-    invoice_inexchange = fields.Boolean(string = "Send to Inexchange", default=True)
-    # ~ inexchange_test_host = fields.Char(string='Test Host',config_parameter='inexchange.test.host',help="Test Host for Inexchange",store=True)
-    # ~ inexchange_real_host = fields.Char(string='Real Host',config_parameter='inexchange.real.host',help="Real Host for Inexchange",store=True)
-    #TODO
-    #test_host = "testapi.inexchange.se"
+
+    attachment_ids = fields.Many2many(
+        'ir.attachment', 'class_id', 'attachment_id', 'Attachments')
+    inexchange_apikey = fields.Char(
+        string='API Key', config_parameter='inexchange.apikey',
+        help="You get this code from your inexchange Account when you "
+             "activate Odoo",
+        store=True)
+    inexchange_client_token = fields.Char(
+        string='Client Token',config_parameter='inexchange.client.token',
+        help="You get this code from your Odoo representative",
+        store=True)
+    invoice_inexchange = fields.Boolean(
+        string="Send to Inexchange", default=True)
+
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
@@ -53,37 +60,37 @@ class ResConfigSettings(models.TransientModel):
     @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-    
+
     # ~ @api.multi
-    def inexchange_request_client_token(self):  
-        client_token = self.env['ir.config_parameter'].sudo().get_param('inexchange.client.token')
+    def inexchange_request_client_token(self):
+        client_token = self.env['ir.config_parameter'].sudo().get_param(
+            'inexchange.client.token')
         if not client_token:
-            api_key = self.env['ir.config_parameter'].sudo().get_param('inexchange.apikey')
+            api_key = self.env['ir.config_parameter'].sudo().get_param(
+                'inexchange.apikey')
             if not api_key:
-                raise Warning("You have to set up API Key for Inexchange, you get that when you activate Odoo in your inexchange-account")
+                raise Warning("You have to set up API Key for Inexchange"
+                              ", you get that when you activate Odoo in "
+                              "your inexchange-account")
             try:
-                _logger.warn('Apikey %s Haze' % api_key)
                 url = "https://testapi.inexchange.se/v1/api/clientTokens/create"
-                r = self.env['res.config.settings'].inexchange_request_api('POST', url,
-                    data={
-                    "erpId": self.env.user.company_id.id,
-                    "validTo": None ,
-                    },)
+                r = self.env['res.config.settings'].inexchange_request_api(
+                    'POST', url, data={
+                        "erpId": self.env.user.company_id.id,
+                        "validTo": None},)
                 r = json.loads(r)
                 client_token = r["token"]
-                _logger.warn('ClientToken %s Haze' % client_token)
-                return client_token
+                self.env['ir.config_parameter'].sudo().set_param(
+                    'inexchange.client.token', client_token)
             except requests.exceptions.RequestException as e:
                 raise Warning('HTTP Request failed %s' % e)
-        
-            
-            
-            
-    
+        return client_token
+
     @api.multi
     def inexchange_request_api(self,request_type,url,data=None):
         # Company (POST /v1/api/companies/register/ HTTP/1.1)
-        api_key = self.env['ir.config_parameter'].sudo().get_param('inexchange.apikey')
+        api_key = self.env['ir.config_parameter'].sudo().get_param(
+            'inexchange.apikey')
         headers = {
                     "APIKey": api_key,
                     "Content-Type": "application/json; charset=utf-8",
@@ -91,31 +98,24 @@ class ResConfigSettings(models.TransientModel):
                     "Content-Length": str(len(data)),
                     "Expect": "100-continue",
                 }
-        _logger.warn('%s Haze Request_type' %request_type)
-        _logger.warn('%s Haze Headers' %headers)
-        _logger.warn('%s %s Haze Url DATA' %(url, data) )
-
         try:
             if request_type == 'POST':
-                r = requests.post(url = url,headers = headers,data = json.dumps(data))
-            # ~ if request_type == 'GET':
-                # ~ r = requests.get(url=url,data = json.dumps(data))
-            _logger.warn('Response HTTP Status Code : {status_code}'.format(status_code=r.status_code))
-            _logger.warn('Response HTTP Response Body : {content}'.format(content=r.content))
+                r = requests.post(
+                    url=url, headers=headers, data=json.dumps(data))
+            _logger.warn(f'Response HTTP Status Code : {r.status_code}')
+            _logger.warn(f'Response HTTP Response Body : {r.content}')
 
-            # ~ raise Warning(r.content)
-            
             if r.status_code in [403]:
                 raise Warning(r.content)
-            
+
         except requests.exceptions.RequestException as e:
             _logger.warn('HTTP Request failed %s' % e)
             raise Warning('HTTP Request failed %s' % e)
         _logger.warn('%s Haze Content 1 ' % r.content) 
         return r.content
-        
+
     @api.multi
-    def inexchange_request_token(self,request_type,url,data=None):
+    def inexchange_request_token(self, request_type, url, data=None):
         # Company (POST /v1/api/companies/register/ HTTP/1.1)
         client_token = self.env['res.config.settings'].inexchange_request_client_token()
         headers = {
@@ -125,9 +125,6 @@ class ResConfigSettings(models.TransientModel):
                     "Accept": "*/*",
                     "Content-Length": str(len(data)) if data else None,
                 }
-        # ~ _logger.warn('%s Haze Request_type' %request_type)
-        # ~ _logger.warn('%s Haze Headers' %headers)
-        # ~ _logger.warn('%s %s Haze Url DATA' %(url, data) )
 
         try:
             if request_type == 'POST':
@@ -137,82 +134,26 @@ class ResConfigSettings(models.TransientModel):
             _logger.warn('Response HTTP Status Code : {status_code}'.format(status_code=r.status_code))
             _logger.warn('Response HTTP Response Body : {content}'.format(content=r.content))
 
-            # ~ raise Warning(r.content)
-            
             if r.status_code in [403]:
                 raise Warning(r.content)
-            
+
         except requests.exceptions.RequestException as e:
             _logger.warn('HTTP Request failed %s' % e)
             raise Warning('HTTP Request failed %s' % e)
-        _logger.warn('%s Haze Content 2' % r.content) 
         return r.content
-    #TODO:how to find a bundary which we can send to Inexchange? Order Number? it depends on us
+
+    # TODO:how to find a bundary which we can send to Inexchange? Order
+    # Number? it depends on us.
     @api.multi
-    def inexchange_invoice_upload(self, request_type, url, data=None):
-        client_token = self.env['res.config.settings'].inexchange_request_client_token()
+    def inexchange_invoice_upload(self, url, data):
+        client_token = self.inexchange_request_client_token()
         url = "https://testapi.inexchange.se/v1/api/documents"
-        for attachment in self.attachment_ids:
-            decoded_data = base64.b64decode(attachment)
-            with open(attachment, 'rb') as f:
-                r = requests.post(url, files={attachment: f})
-            _logger.debug('Haze.text %s' %attachment)
-        # ~ document_name = self.env['ir.attachment'].res_name
-        # ~ _logger.warn('Hazedocument %s' %document_name)
-        # ~ attachments = self.env['ir.attachment'].create({
-            # ~ 'res_model':self._name,
-            # ~ 'res_id': self.id,
-            # ~ 'datas': base64.b64encode(data),
-            # ~ }
-        # ~ )
-        # ~ attachment = self.env['ir.attachment'].create({
-            # ~ 'res_model': self._name,
-            # ~ 'res_id': self.id,
-            # ~ 'datas': base64.b64encode(data),
-            # ~ 'name': 'UBL-Invoice-2.1.xml',
-            # ~ 'datas_fname': 'UBL-Invoice-2.1.xml',
-        # ~ })
-        # ~ _logger.warn('Hazeattachment %s' %attachment)
-        # ~ _logger.warn('%s Hazelocation**' %client_token)
-        # ~ headers = {
-                    # ~ "ClientToken": client_token,
-                    # ~ "Host": "testapi.inexchange.se",
-                    # ~ "Accept": "*/*",
-                    # ~ "Content-Type": "multipart/form-data; boundary=%s" %boundary,
-                    # ~ "Content-Length": str(len(data)) if data else None,
-                    # ~ "Content-Length": str(len(base64.b64encode(data))) if data else None,
-                    
-                    # ~ "boundary":{
-                    # ~ "Content-Disposition": 'form-data; name="File"; content_disposition(name))',
-                    # ~ #filename we have to set as well,
-                    # ~ "Content-Type": "application/xml",
-                    # ~ },
-                # ~ }
-        # ~ _logger.warn('%s Hazelocation*' %client_token)
-        # ~ _logger.warn('%s Haze upload Request_type' %request_type)
-        # ~ _logger.warn('%s Haze upload Headers' %headers)
-        # ~ _logger.warn('%s %s Haze upload Url DATA' %(url, data) )
-        # ~ try:
-            # ~ if request_type == 'POST':
-                # ~ r = requests.post(url = url,headers = headers,data = data)
-                # ~ r = requests.post(url = url,headers = headers,data = base64.b64encode(data))
-            # ~ _logger.warn('Response HTTP Status Code : {status_code}'.format(status_code=r.status_code))
-            # ~ _logger.warn('Response HTTP Response Body : {content}'.format(content=r.content))
-
-            # ~ raise Warning(r.content)
-            
-            # ~ if r.status_code in [403]:
-                # ~ raise Warning(r.content)
-            
-        # ~ except requests.exceptions.RequestException as e:
-            # ~ _logger.warn('HTTP Request failed %s' % e)
-            # ~ raise Warning('HTTP Request failed %s' % e)
-        # ~ _logger.warn('%s Haze upload Content 2' % r.content) 
-        # ~ return r.content
-        
-        
-        
-            
-        
-
-
+        header = {
+            'ClientToken': client_token,
+            'ContentDisposition': 'attachement; filename="invoice.xls"',
+            'host': 'testapi.inexchange.se'
+            }
+        result = requests.post(
+            url, headers=header, files={'file': ('invoice.xls', data, 'application/xml')})
+        raise Warning(result.headers)
+        return result
