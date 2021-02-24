1cbe66f0bef9c9cca96ec42562d16302151328ec
Lukas Pohlman <lukas.pohlman@vertel.se>
2021-02-24 11:09:13 +0000

    tabs to spaces

--- account_fortnox/models/res_partner.py
+++ account_fortnox/models/res_partner.py
@@ -47,72 +47,72 @@ class Partner(models.Model):
                         _logger.warn("~ NICE: partner.ref was set to %s" % customer['CustomerNumber'])
                         #partner.ref = customer['CustomerNumber']
 
-	def partner_create(self):
-		# Customer (PUT https://api.fortnox.se/3/customers)
-		for partner in self:
-			if not partner.commercial_partner_id.ref:
-				url = "https://api.fortnox.se/3/customers"
-				r = self.env.user.company_id.fortnox_request('post', url,
-					data={
-						"Customer": {
-							"Address1": partner.street,
-							"Address2": partner.street2,
-							"City": partner.city,
-							"Comments": partner.comment,
-							"CountryCode": "SE",
-							"Currency": "SEK",
-							# ~ "CustomerNumber": partner.commercial_partner_id.id,
-							"Email": partner.email or None,
-							"Name": partner.commercial_partner_id.name,
-							"OrganisationNumber": partner.commercial_partner_id.company_registry,
-							"OurReference": partner.commercial_partner_id.user_id.name,
-							"Phone1": partner.commercial_partner_id.phone,
-							"Phone2": None,
-							"PriceList": "A",
-							"ShowPriceVATIncluded": False,
-							# ~ "TermsOfPayment": partner.commercial_partner_id.property_payment_term_id.name,
-							"Type": "COMPANY",
-							"VATNumber": partner.commercial_partner_id.vat,
-							"VATType": "SEVAT",
-							"WWW": partner.commercial_partner_id.website,
-							"YourReference": partner.name,
-							"ZipCode": partner.zip,
-						}
-					})
+    def partner_create(self):
+        # Customer (PUT https://api.fortnox.se/3/customers)
+        for partner in self:
+            if not partner.commercial_partner_id.ref:
+                url = "https://api.fortnox.se/3/customers"
+                r = self.env.user.company_id.fortnox_request('post', url,
+                    data={
+                        "Customer": {
+                            "Address1": partner.street,
+                            "Address2": partner.street2,
+                            "City": partner.city,
+                            "Comments": partner.comment,
+                            "CountryCode": "SE",
+                            "Currency": "SEK",
+                            # ~ "CustomerNumber": partner.commercial_partner_id.id,
+                            "Email": partner.email or None,
+                            "Name": partner.commercial_partner_id.name,
+                            "OrganisationNumber": partner.commercial_partner_id.company_registry,
+                            "OurReference": partner.commercial_partner_id.user_id.name,
+                            "Phone1": partner.commercial_partner_id.phone,
+                            "Phone2": None,
+                            "PriceList": "A",
+                            "ShowPriceVATIncluded": False,
+                            # ~ "TermsOfPayment": partner.commercial_partner_id.property_payment_term_id.name,
+                            "Type": "COMPANY",
+                            "VATNumber": partner.commercial_partner_id.vat,
+                            "VATType": "SEVAT",
+                            "WWW": partner.commercial_partner_id.website,
+                            "YourReference": partner.name,
+                            "ZipCode": partner.zip,
+                        }
+                    })
                 r = json.loads(r)
                 partner.commercial_partner_id.ref = r["Customer"]["CustomerNumber"]
 
-	@api.multi
-	def partner_update(self):
-		# Customer (PUT https://api.fortnox.se/3/customers)
-		for partner in self:
-			if partner.commercial_partner_id.ref:
-				url = "https://api.fortnox.se/3/customers/%s" % partner.commercial_partner_id.ref
-				""" r = response """
-				r = self.env.user.company_id.fortnox_request('put', url,
-					data={
-						"Customer": {
-							"Address1": partner.street,
-							"Address2": partner.street2,
-							"City": partner.city,
-							"Comments": partner.comment,
-							"CountryCode": "SE",
-							"Currency": "SEK",
-							# ~ "CustomerNumber": partner.commercial_partner_id.ref,
-							"Email": partner.email or None,
-							"Name": partner.commercial_partner_id.name,
-							"OrganisationNumber": partner.commercial_partner_id.company_registry,
-							"OurReference": partner.commercial_partner_id.user_id.name,
-							"Phone1": partner.commercial_partner_id.phone,
-							"Phone2": None,
-							"PriceList": "A",
-							"ShowPriceVATIncluded": False,
-							# ~ "TermsOfPayment": partner.commercial_partner_id.property_payment_term_id.name,
-							"Type": "COMPANY",
-							"VATNumber": partner.commercial_partner_id.vat,
-							"VATType": "SEVAT",
-							"WWW": partner.commercial_partner_id.website,
-							"YourReference": partner.name,
-							"ZipCode": partner.zip,
-						}
-					})
+    @api.multi
+    def partner_update(self):
+        # Customer (PUT https://api.fortnox.se/3/customers)
+        for partner in self:
+            if partner.commercial_partner_id.ref:
+                url = "https://api.fortnox.se/3/customers/%s" % partner.commercial_partner_id.ref
+                """ r = response """
+                r = self.env.user.company_id.fortnox_request('put', url,
+                    data={
+                        "Customer": {
+                            "Address1": partner.street,
+                            "Address2": partner.street2,
+                            "City": partner.city,
+                            "Comments": partner.comment,
+                            "CountryCode": "SE",
+                            "Currency": "SEK",
+                            # ~ "CustomerNumber": partner.commercial_partner_id.ref,
+                            "Email": partner.email or None,
+                            "Name": partner.commercial_partner_id.name,
+                            "OrganisationNumber": partner.commercial_partner_id.company_registry,
+                            "OurReference": partner.commercial_partner_id.user_id.name,
+                            "Phone1": partner.commercial_partner_id.phone,
+                            "Phone2": None,
+                            "PriceList": "A",
+                            "ShowPriceVATIncluded": False,
+                            # ~ "TermsOfPayment": partner.commercial_partner_id.property_payment_term_id.name,
+                            "Type": "COMPANY",
+                            "VATNumber": partner.commercial_partner_id.vat,
+                            "VATType": "SEVAT",
+                            "WWW": partner.commercial_partner_id.website,
+                            "YourReference": partner.name,
+                            "ZipCode": partner.zip,
+                        }
+                    })
