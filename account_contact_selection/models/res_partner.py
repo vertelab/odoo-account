2f83fc06465215a0da8d75058574477dd5fd453a
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2021-10-27 13:10:11 +0000

    account_contact_selection:Added a module that removes the limitations when selecting an account on a contact

---
+++ account_contact_selection/models/res_partner.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Enterprise Management Solution, third party addon
+#    Copyright (C) 2017 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import api, fields, models, _, exceptions
+from odoo.osv import expression
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import Warning
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    property_account_payable_id = fields.Many2one('account.account', company_dependent=True,
+        string="Account Payable",
+        domain="[]",
+        help="This account will be used instead of the default one as the payable account for the current partner",
+        required=True)
+    property_account_receivable_id = fields.Many2one('account.account', company_dependent=True,
+        string="Account Receivable",
+        domain="[]",
+        help="This account will be used instead of the default one as the receivable account for the current partner",
+        required=True)
