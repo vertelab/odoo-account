24681ff3cea04885b743e9749a2ad0dff4a0af55
iammiracle <miracleaayodele@gmail.com>
2021-11-03 13:30:55 +0100

    added multiple analytic account ids

---
+++ account_analytic_move_ids/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Enterprise Management Solution, third party addon
+#    Copyright (C) 2017 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Account Analytic Move',
+    'version': '14.0.0.0.1',
+    'summary': 'Account Analytic Move',
+    'description': """
+Account Analytic Name
+========================================================
+Changes the behaviour of account analaytic move
+    """,
+    'category': 'Accounting',
+    'author': 'Vertel AB',
+    'website': 'https://www.vertel.se',
+    'images': [],
+    'depends': ['analytic', 'account'],
+    'data': [
+        'views/account_move_views.xml'
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
