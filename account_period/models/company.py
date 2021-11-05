2fb1db729598e585e623a4632278aa1164a06deb
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2021-11-05 09:23:00 +0000

    account_period: Removed a line that creates a account.move since this breaks if we haven't created account.periods yet.

--- account_period/models/company.py
+++ account_period/models/company.py
@@ -7,8 +7,8 @@ class ResCompany(models.Model):
     @api.model
     def setting_init_fiscal_year_action(self):
         """ Called by the 'Fiscal Year Opening' button of the setup bar."""
-        company = self.env.company
-        company.create_op_move_if_non_existant()
+        #company = self.env.company
+        #company.create_op_move_if_non_existant()
         # new_wizard = self.env['account.fiscalyear'].create({'company_id': company.id})
         view_id = self.env.ref('account_period.view_account_fiscalyear_form').id
 
