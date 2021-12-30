f8f18bc1b515cf2358e293aef60218899766c4d4
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-12-30 10:05:04 +0100

    Update __manifest__.py

--- account_contact_selection/__manifest__.py
+++ account_contact_selection/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2017 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,20 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name' : 'Remove Account Selection Limitation On Contacts ',
-    'version' : '1.0',
-    'summary': '',
+    'name': 'Account: Remove Account Selection...',
+    'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Remove Account Selection Limitation On Contacts',
+    'category': 'Accounting',
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/account/remove-account',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
     'description': """
 
     """,
-    'category': 'Accounting',
-    'author': 'Vertel AB',
-    'website': 'http://www.vertel.se',
-    'images': [],
     'depends': ['contacts','account'],
     'data': [
         #'views/account_view.xml',
