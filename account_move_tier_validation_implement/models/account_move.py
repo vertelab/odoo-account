43fc04cf1612f24a7dd7712ce873aee7ea0f012a
Marcus Jakobsson <jakmar@odooutv14.vertel.se>
2022-03-07 14:25:51 +0000

    move tier validtaion

---
+++ account_move_tier_validation_implement/models/account_move.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    move_tier_validator = fields.Many2one('res.partner', tracking=True )
+
+    
