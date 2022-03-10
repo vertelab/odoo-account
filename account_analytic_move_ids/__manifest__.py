4cf46c10295809d6e0f1188d3af7e78bb9485b54
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-03-10 14:14:49 +0000

    account_analytic_move_ids:Made it so that account default rules are aplied on all account move lines, even them that are exluded from the invoice line tab.

--- account_analytic_move_ids/__manifest__.py
+++ account_analytic_move_ids/__manifest__.py
@@ -20,12 +20,12 @@
 ##############################################################################
 
 {
-    'name': 'Account Analytic Move Ids',
+    'name': 'Account Default Analytic Move Line Ids',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     'category': 'Accounting',
-    'description': '',
+    'description': "Analytic Default rules aren't applied on line ids on a account move, so typically tax lines that odoo generates which aren't in the invoice line tab. This module changes that, now Analytic Default rules are applied to these kinds of lines as well",
     #'sequence': '1'
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-account/account-analytic-move-ids',
--- account_analytic_move_ids/models/account_move.py
+++ account_analytic_move_ids/models/account_move.py
@@ -10,7 +10,21 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.depends('product_id', 'account_id', 'partner_id', 'date')
+    # ~ @api.depends('product_id', 'account_id', 'partner_id', 'date')
+    # ~ def _compute_analytic_account_id_background_lines(self):
+        # ~ for record in self:
+                # ~ rec = self.env['account.analytic.default'].account_get(
+                    # ~ product_id=record.product_id.id,
+                    # ~ partner_id=record.partner_id.commercial_partner_id.id or record.move_id.partner_id.commercial_partner_id.id,
+                    # ~ account_id=record.account_id.id,
+                    # ~ user_id=record.env.uid,
+                    # ~ date=record.date,
+                    # ~ company_id=record.move_id.company_id.id
+                # ~ )
+                # ~ if rec:
+                    # ~ record.analytic_account_id = rec.analytic_id
+                    
+    # ~ @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id_background_lines(self):
         for record in self:
                 rec = self.env['account.analytic.default'].account_get(
@@ -25,3 +39,17 @@ class AccountMoveLine(models.Model):
                     record.analytic_account_id = rec.analytic_id
 
     
+    @api.depends('product_id', 'account_id', 'partner_id', 'date')
+    def _compute_analytic_account(self):
+        for record in self:
+                rec = self.env['account.analytic.default'].account_get(
+                    product_id=record.product_id.id,
+                    partner_id=record.partner_id.commercial_partner_id.id or record.move_id.partner_id.commercial_partner_id.id,
+                    account_id=record.account_id.id,
+                    user_id=record.env.uid,
+                    date=record.date,
+                    company_id=record.move_id.company_id.id
+                )
+                if rec:
+                    record.analytic_account_id = rec.analytic_id
+                    record.analytic_tag_ids = rec.analytic_tag_ids
