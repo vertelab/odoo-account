dea46cad29a5db581b3277cf4153747f730fb568
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-03-11 10:28:35 +0000

    account_analytic_default_all_move_lines: analytic rules are now applied on generated tax lines aswell.

--- account_analytic_default_all_move_lines/models/account_move.py
+++ account_analytic_default_all_move_lines/models/account_move.py
@@ -10,20 +10,12 @@ _logger = logging.getLogger(__name__)
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    # ~ @api.depends('product_id', 'account_id', 'partner_id', 'date')
-    # ~ def _compute_analytic_account_id_background_lines(self):
-        # ~ for record in self:
-                # ~ rec = self.env['account.analytic.default'].account_get(
-                    # ~ product_id=record.product_id.id,
-                    # ~ partner_id=record.partner_id.commercial_partner_id.id or record.move_id.partner_id.commercial_partner_id.id,
-                    # ~ account_id=record.account_id.id,
-                    # ~ user_id=record.env.uid,
-                    # ~ date=record.date,
-                    # ~ company_id=record.move_id.company_id.id
-                # ~ )
-                # ~ if rec:
-                    # ~ record.analytic_account_id = rec.analytic_id
-                    
+    @api.depends('tax_repartition_line_id.invoice_tax_id', 'tax_repartition_line_id.refund_tax_id') #Otherwise the tax lines wont have analytic accounts applied on itself
+    def _compute_tax_line_id(self):
+            res = super(AccountMoveLine, self)._compute_tax_line_id()
+            self._compute_analytic_account()
+            return res
+            
     # ~ @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id_background_lines(self):
         for record in self:
@@ -41,6 +33,8 @@ class AccountMoveLine(models.Model):
     
     @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id(self):
+        _logger.warning("_compute_analytic_account")
+        _logger.warning("_compute_analytic_account")
         for record in self:
                 rec = self.env['account.analytic.default'].account_get(
                     product_id=record.product_id.id,
@@ -50,6 +44,8 @@ class AccountMoveLine(models.Model):
                     date=record.date,
                     company_id=record.move_id.company_id.id
                 )
+                _logger.warning(f"analytictest{record=}")
+                _logger.warning(f"analytictest{rec=}")
                 if rec:
                     record.analytic_account_id = rec.analytic_id
                     union_record_ids=rec.analytic_tag_ids | record.analytic_tag_ids
