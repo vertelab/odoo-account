6b8b7e133af5e10a4d2ebed6b4f9e0a3839d830c
Miracle <miracleaayodele@gmail.com>
2022-05-19 14:22:31 +0000

    payment filter with currency

---
+++ payment_order_filter/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account Payment Line Create',
+    'version': '14.0.0.0.1',
+    'summary': 'Override Account Payment Line Create',
+    'category': 'Accounting',
+    'author': 'Vertel AB',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'description': """
+        Override Account Payment Line Create to take currency into consideration
+    """,
+    'depends': ['account_payment_order'],
+    'data': [
+        'views/account_payment_line_create_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ payment_order_filter/models/account_payment_line_create.py
@@ -0,0 +1,113 @@
+from locale import currency
+from odoo import _, api, fields, models
+
+class AccountPaymentOrder(models.Model):
+    _inherit = "account.payment.order"
+
+    def account_payment_line_create_action(self):
+        action = {
+            "name": _("Create Transactions from Move Lines"),
+            "type": "ir.actions.act_window",
+            "res_model": "account.payment.line.create",
+            "view_mode": "form",
+            "target": "new",
+            "context": self._context,
+        }
+        return action
+
+
+
+
+class AccountPaymentLineCreate(models.TransientModel):
+    _inherit  = "account.payment.line.create"
+
+    def _get_default_currency(self):
+        ''' Get the default currency from either the journal, either the default journal's company. '''
+        params = self._context.get('params')
+        order_id = self.env[params.get('model')].browse(params.get('id'))
+        return order_id.journal_id.currency_id or order_id.journal_id.company_id.currency_id
+
+
+    currency_id = fields.Many2one('res.currency', string='Currency', default=_get_default_currency)
+
+    def _prepare_move_line_domain(self):
+        self.ensure_one()
+        domain = [
+            ("reconciled", "=", False),
+            ("company_id", "=", self.order_id.company_id.id),
+        ]
+        if self.journal_ids:
+            domain += [("journal_id", "in", self.journal_ids.ids)]
+        if self.partner_ids:
+            domain += [("partner_id", "in", self.partner_ids.ids)]
+        if self.target_move == "posted":
+            domain += [("move_id.state", "=", "posted")]
+        if not self.allow_blocked:
+            domain += [("blocked", "!=", True)]
+        if self.currency_id:
+            domain += [("currency_id", "=", self.currency_id.id)]
+        if self.date_type == "due":
+            domain += [
+                "|",
+                ("date_maturity", "<=", self.due_date),
+                ("date_maturity", "=", False),
+            ]
+        elif self.date_type == "move":
+            domain.append(("date", "<=", self.move_date))
+        if self.invoice:
+            domain.append(
+                (
+                    "move_id.move_type",
+                    "in",
+                    ("in_invoice", "out_invoice", "in_refund", "out_refund"),
+                )
+            )
+        if self.payment_mode:
+            if self.payment_mode == "same":
+                domain.append(
+                    ("payment_mode_id", "=", self.order_id.payment_mode_id.id)
+                )
+            elif self.payment_mode == "same_or_null":
+                domain += [
+                    "|",
+                    ("payment_mode_id", "=", False),
+                    ("payment_mode_id", "=", self.order_id.payment_mode_id.id),
+                ]
+
+        if self.order_id.payment_type == "outbound":
+            # For payables, propose all unreconciled credit lines,
+            # including partially reconciled ones.
+            # If they are partially reconciled with a supplier refund,
+            # the residual will be added to the payment order.
+            #
+            # For receivables, propose all unreconciled credit lines.
+            # (ie customer refunds): they can be refunded with a payment.
+            # Do not propose partially reconciled credit lines,
+            # as they are deducted from a customer invoice, and
+            # will not be refunded with a payment.
+            domain += [
+                ("credit", ">", 0),
+                ("account_id.internal_type", "in", ["payable", "receivable"]),
+            ]
+        elif self.order_id.payment_type == "inbound":
+            domain += [
+                ("debit", ">", 0),
+                ("account_id.internal_type", "in", ["receivable", "payable"]),
+            ]
+        # Exclude lines that are already in a non-cancelled
+        # and non-uploaded payment order; lines that are in a
+        # uploaded payment order are proposed if they are not reconciled,
+        paylines = self.env["account.payment.line"].search(
+            [
+                ("state", "in", ("draft", "open", "generated")),
+                ("move_line_id", "!=", False),
+            ]
+        )
+        if paylines:
+            move_lines_ids = [payline.move_line_id.id for payline in paylines]
+            domain += [("id", "not in", move_lines_ids)]
+        return domain
+
+
+
+    
