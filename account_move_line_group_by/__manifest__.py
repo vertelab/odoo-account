8bd4e7e25433eec0a43b69b195b12d23cb8b6a9e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-16 11:53:10 +0200

    Update __manifest__.py
    fixing manifest

--- account_move_line_group_by/__manifest__.py
+++ account_move_line_group_by/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2017 Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,21 +15,25 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    'name' : 'Journal Lines Group By',
-    'version' : '14.0',
-    'summary': '',
-    'description': """
-     Adds new options to groups journal lines by
 
-    """,
+{
+    'name': 'Account: Journal Lines Group By',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds new options to groups journal lines by.',
     'category': 'Accounting',
+    'description': 'Adds new options to groups journal lines by.',
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'http://www.vertel.se',
-    'images': [],
+    'website': 'https://vertel.se/apps/odoo-account/account_move_line_group_by',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account.git',
     'depends': ['account'],
     'data': [
         'views/account_move_line_view.xml',
