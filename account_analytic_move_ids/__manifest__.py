3fce1e6f576de773d629dde43c31e844c61848c0
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-26 11:06:33 +0200

    Update __manifest__.py

--- account_analytic_move_ids/__manifest__.py
+++ account_analytic_move_ids/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Account Analytic Move Ids',
+    'name': 'Account: Analytic Move Ids',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
