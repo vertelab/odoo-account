a646cba08f306bbc64cb9a48e8e95b5fd269511c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-26 11:05:07 +0200

    Update __manifest__.py

--- account_analytic_line_project/__manifest__.py
+++ account_analytic_line_project/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Account Analytic Line Project',
+    'name': 'Account: Analytic Line Project',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds project to account analytic line',
