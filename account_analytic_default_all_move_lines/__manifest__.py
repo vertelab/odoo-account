b1e24aadfeee79a51f152e3687031f6ca369c5fa
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-26 11:04:24 +0200

    Update __manifest__.py

--- account_analytic_default_all_move_lines/__manifest__.py
+++ account_analytic_default_all_move_lines/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Account Default Analytic Move Line Ids',
+    'name': 'Account: Default Analytic Move Line Ids',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
