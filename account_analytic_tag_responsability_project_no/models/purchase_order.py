58456fed549e3a18882207ab6c24fe71755b0f32
iammiracle <miracleaayodele@gmail.com>
2022-11-24 13:03:02 +0100

    fixed AOR on purchase line

--- account_analytic_tag_responsability_project_no/models/purchase_order.py
+++ account_analytic_tag_responsability_project_no/models/purchase_order.py
@@ -8,8 +8,8 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    # ~ def button_confirm(self):
-        # ~ if len(self.order_line.filtered(lambda x: not x.analytic_tag_ids)) > 0:
-            # ~ raise ValidationError(_("Kindly select analytic tag for all line items"))
-        # ~ res = super(PurchaseOrder, self).button_confirm()
-        # ~ return res
+    def button_confirm(self):
+        if len(self.order_line.filtered(lambda line: not line.area_of_responsibility)) > 0:
+            raise ValidationError(_("Kindly select area of responsibility tag for all line items"))
+        res = super(PurchaseOrder, self).button_confirm()
+        return res
