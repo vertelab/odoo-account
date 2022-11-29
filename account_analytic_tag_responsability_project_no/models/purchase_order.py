750d9a406b0ef37876c09ca38edbab13f2051445
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2022-11-29 08:21:31 +0100

    Update purchase_order.py

--- account_analytic_tag_responsability_project_no/models/purchase_order.py
+++ account_analytic_tag_responsability_project_no/models/purchase_order.py
@@ -9,7 +9,7 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def button_confirm(self):
-        if len(self.order_line.filtered(lambda line: not line.area_of_responsibility)) > 0:
+        if len(self.order_line.filtered(lambda line: not line.area_of_responsibility and not line.display_type)) > 0:
             raise ValidationError(_("Kindly select area of responsibility tag for all line items"))
         res = super(PurchaseOrder, self).button_confirm()
         return res
