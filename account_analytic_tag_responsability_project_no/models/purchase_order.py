6d8802e5c17f2edbb09acec704ec77b960eb1948
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-12-16 10:53:04 +0000

    account_analytic_tag_responsability_project_no: invoices created from a sale/purchase order gets its area/project tags filled by the sale/purchase order line.

--- account_analytic_tag_responsability_project_no/models/purchase_order.py
+++ account_analytic_tag_responsability_project_no/models/purchase_order.py
@@ -13,3 +13,16 @@ class PurchaseOrder(models.Model):
             raise ValidationError(_("Kindly select a Cost Center tag for all line items"))
         res = super(PurchaseOrder, self).button_confirm()
         return res
+        
+    def action_create_invoice(self):
+        _logger.warning("create_invoices"*100)
+        vals = super().action_create_invoice()
+        
+        for order in self:
+            for invoice in order.invoice_ids:
+                for line in invoice.line_ids:
+                    if line.purchase_line_id:
+                            line.project_no = line.purchase_line_id.project_no
+                            line.area_of_responsibility = line.purchase_line_id.area_of_responsibility
+        
+        return vals
---
+++ account_analytic_tag_responsability_project_no/wizard/sale_make_invoice_advance.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import time
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    def create_invoices(self):
+        _logger.warning("create_invoices"*100)
+        vals = super().create_invoices()
+        
+        for sale_order in self.env['sale.order'].browse(self._context.get('active_ids', [])):
+            for invoice in sale_order.invoice_ids:
+                for line in invoice.line_ids:
+                    if line.sale_line_ids:
+                        for sale_line in line.sale_line_ids:
+                            if sale_line.project_no:
+                                line.project_no = sale_line.project_no
+                            if sale_line.area_of_responsibility:
+                                line.area_of_responsibility = sale_line.area_of_responsibility
+        return vals
+        
+        
+  
