600e7ac43123bbeda4682fac23c41e7e000346d2
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-02-13 15:36:52 +0100

    Update __manifest__.py

--- account_contact_selection/__manifest__.py
+++ account_contact_selection/__manifest__.py
@@ -27,7 +27,7 @@
     'category': 'Accounting',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-account/account-contact-selection',
+    'website': 'https://vertel.se/apps/odoo-account/account_contact_selection',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
