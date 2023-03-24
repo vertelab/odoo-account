7c27c51f5a6ed91e05410068223c9fd7800b77c4
Your Name <you@example.com>
2023-03-24 13:30:53 +0000

    account_period:fixed recuresion error

--- account_period/models/account.py
+++ account_period/models/account.py
@@ -498,9 +498,20 @@ class AccountInvoice(models.Model):
                     **locals()))
         return super(AccountInvoice, self).action_invoice_open()
     
-class account_payment(models.Model):
-    _inherit = "account.payment"
+#class account_payment(models.Model):
+#    _inherit = "account.payment"
 
+#    @api.multi
+#    def post(self):
+#        context_copy = self.env.context.copy()
+#        context_copy.update({'check_move_period_validity': False})
+#        self_with_context = self.with_context(context_copy)
+#        res = super(account_payment, self).with_context(context_copy).post()
+#        #res = super(account_payment, self_with_context).post()
+#        return res 
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
     @api.multi
     def post(self):
         context_copy = self.env.context.copy()
@@ -508,8 +519,6 @@ class account_payment(models.Model):
         self_with_context = self.with_context(context_copy)
         #res = super(account_payment, self).with_context(context_copy).post()
         res = super(AccountPayment, self_with_context).post()
-        return res 
-
-        
+        return res       
 
 
