34d24f4e05adf93c5546ccf70dcb093b49f8f794
Björn Hayer <bjorn.hayer@vertel.se>
2023-05-23 08:58:31 +0000

    Fixed issue with module preventing several sale orders being merged into fewer amount of invoices. Moved the field linking back to teh sale order. Chatter already has this functionality.

--- account_move_contact/models/account_move.py
+++ account_move_contact/models/account_move.py
@@ -6,5 +6,4 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    sale_order_id = fields.Many2one('sale.order', string='Related Sale Order')
-    contact_id = fields.Many2one(related='sale_order_id.partner_id', string='Contact', readonly=True)
+    contact_id = fields.Many2one('res.partner', string='Contact')
--- account_move_contact/models/sale_order.py
+++ account_move_contact/models/sale_order.py
@@ -7,14 +7,17 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _create_invoices(self, grouped=False, final=False, date=None):
-        moves = self.env['account.move']
-        for record in self:
-            move = super(SaleOrder, record)._create_invoices(grouped, final, date)
-            move.sale_order_id = record.id
-            if move:
-                moves += move
+        moves = super()._create_invoices(grouped, final, date)
+
+        # Goes through all the created Invoices (account.moves), and finds
+        # the customer in the originating sale order(s), and puts said
+        # customer in the contact field in the account.move
+        for move in moves:
+            if move.invoice_origin:
+                order_name = move.invoice_origin.split(',')[0]
+                for order in self:
+                    if order.name == order_name:
+                        move.contact_id = order.partner_id.id
+                        break
+                        
         return moves
-        #if len(moves) > 0:
-        #    return moves
-        #else:
-        #    return False
