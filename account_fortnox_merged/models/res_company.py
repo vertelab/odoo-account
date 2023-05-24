fa30af51b345dd4cee9904939104e58ec835dadc
Dmitri Iselund <dmitri.iselund@vertel.se>
2023-05-24 14:41:12 +0000

    account_fortnox_merged: added a function to refresh access_token, added a button to call that function

--- account_fortnox_merged/models/res_company.py
+++ account_fortnox_merged/models/res_company.py
@@ -41,6 +41,70 @@ class res_company(models.Model):
     fortnox_client_secret = fields.Char(string='Client Secret', help="You get this code from your Odoo representative", store=True)
     fortnox_access_token = fields.Text(string='Access Token', help="With autorization code and client secret you generate this code ones", store=True)
     fortnox_client_id = fields.Char(string='Client ID', help="The public ID of the integration", store=True)
+    fortnox_token_expiration = fields.Char(store=True)
+    fortnox_refresh_token = fields.Text(store=True)
+
+    def fortnox_get_access_token_new(self):
+        if not self.fortnox_access_token:
+            if not self.fortnox_authorization_code:
+                raise UserError("You have to set up Authorization_token for FortNox, you get that when you activate Odoo in your FortNox-account")
+            if not self.fortnox_client_secret:
+                raise UserError("You have to set up Client_secret for FortNox, you get that when you activate Odoo in your FortNox-account")
+            if not self.fortnox_client_id:
+                raise UserError("You have to supply the client ID of the integration")
+            try:
+                credentials_encoded = f"{self.fortnox_client_id}:{self.fortnox_client_secret}".encode("utf-8")
+                credentials_b64encoded = base64.b64encode(credentials_encoded).decode("utf-8")
+                r = requests.post(
+                    url="https://apps.fortnox.se/oauth-v1/token",
+                    headers = {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Authorization": f"Basic {credentials_b64encoded}",
+                    },
+                    data = {
+                        'grant_type': 'authorization_code',
+                        'code': self.fortnox_authorization_code,
+                        'redirect_uri': 'https://vertel.se'
+                    }
+                )
+                if r.status_code not in (200, 201, 204):
+                    raise UserError(f'FortNox: StatusCode:{r.status_code}, \n'
+                                    f'Content:{r.content}')
+                auth_rec = json.loads(r.content)
+                self.fortnox_access_token = auth_rec.get('access_token')
+                self.fortnox_refresh_token = auth_rec.get('refresh_token')
+                self.fortnox_token_expiration = time.time() + auth_rec.get('expires_in')
+            except requests.exceptions.RequestException as e:
+                raise UserError('HTTP Request failed %s' % e)
+        else:
+            if self.fortnox_token_expiration and time.time() >= self.fortnox_token_expiration:
+                # Access token has expired, refresh using the refresh token
+                if not self.fortnox_refresh_token:
+                    raise UserError('Refresh token is missing')
+                try:
+                    credentials_encoded = f"{self.fortnox_client_id}:{self.fortnox_client_secret}".encode("utf-8")
+                    credentials_b64encoded = base64.b64encode(credentials_encoded).decode("utf-8")
+                    r = requests.post(
+                        url="https://apps.fortnox.se/oauth-v1/token",
+                        headers = {
+                            "Content-Type": "application/x-www-form-urlencoded",
+                            "Authorization": f"Basic {credentials_b64encoded}",
+                        },
+                        data = {
+                            'grant_type': 'refresh_token',
+                            'refresh_token': self.fortnox_refresh_token,
+                        }
+                    )
+                    if r.status_code not in (200, 201, 204):
+                        raise UserError(f'FortNox: StatusCode:{r.status_code}, \n'
+                                        f'Content:{r.content}')
+                    auth_rec = json.loads(r.content)
+                    self.fortnox_access_token = auth_rec.get('access_token')
+                    self.fortnox_refresh_token = auth_rec.get('refresh_token')
+                    self.fortnox_token_expiration = time.time() + auth_rec.get('expires_in')
+                except requests.exceptions.RequestException as e:
+                    raise UserError('HTTP Request failed %s' % e)
+        return
 
     def fortnox_get_access_token(self):
         if not self.fortnox_access_token:
@@ -74,9 +138,10 @@ class res_company(models.Model):
                                     f'Content:{r.content}')
                 auth_rec = json.loads(r.content)
                 _logger.warning(f"{auth_rec=}")
-                _logger.warning(f"{auth_rec.get('access_token')=}")
+                # _logger.warning(f"{auth_rec.get('access_token')=}")
                 self.fortnox_access_token = auth_rec.get('access_token')
-                _logger.warning(f"{self.fortnox_access_token=}")
+                self.fortnox_refresh_token = auth_rec.get('refresh_token')
+                # _logger.warning(f"{self.fortnox_access_token=}")
                 # msg = _("New Access Token {token}").format(self.fortnox_access_token)
                     
                 # self.message_post(
@@ -85,6 +150,41 @@ class res_company(models.Model):
                 raise UserError('HTTP Request failed %s' % e)
         else:
             raise UserError('Access Token already fetched')
+            
+    def fortnox_refresh_access_token(self):
+        try:
+            credentials_encoded = f"{self.fortnox_client_id}:{self.fortnox_client_secret}".encode("utf-8")
+            credentials_b64encoded = base64.b64encode(credentials_encoded).decode("utf-8")
+            r = requests.post(
+                url="https://apps.fortnox.se/oauth-v1/token",
+                headers = {
+                   # "ClientId": self.fortnox_client_id,
+                   # "ClientSecret": self.fortnox_client_secret,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                   # "Accept": "application/json",
+                    "Authorization": f"Basic {credentials_b64encoded}", 
+                },
+                data = {
+                   'grant_type': 'refresh_token',
+                   'refresh_token': self.fortnox_refresh_token,
+                }
+            )
+            
+#            _logger.warning(f"{credentials_b64encoded=} {self.fortnox_refresh_token=}")
+            
+            if r.status_code not in (200, 201, 204):
+                raise UserError(f'FortNox: StatusCode:{r.status_code}, \n'
+                                f'Content:{r.content}')
+            auth_rec = json.loads(r.content)
+            _logger.warning(f"{auth_rec=}")
+            self.fortnox_access_token = auth_rec.get('access_token')
+            self.fortnox_refresh_token = auth_rec.get('refresh_token')
+            # msg = _("New Access Token {token}").format(self.fortnox_access_token)
+                
+            # self.message_post(
+            #     body=msg, subject=None, message_type='notification')
+        except requests.exceptions.RequestException as e:
+            raise UserError('HTTP Request failed %s' % e)
 
     def fortnox_request(self, request_type, url, data=None, raise_error=True):
         # Customer (POST https://api.fortnox.se/3/customers)
