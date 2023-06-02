ad7a2cb7038a81c5276a50f3a609d92b3b461b53
iammiracle <miracleaayodele@gmail.com>
2023-06-02 11:12:45 +0200

    pending tier review mail reminder

---
+++ tier_validation_reminder/models/tier.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api, _
+
+
+class ReviewTier(models.Model):
+    _inherit = "tier.review"
+
+    def _review_reminder(self):
+        tier_review_ids = self.env[self._name].search([("status", "=", "pending")])
+        template = self.env.ref('tier_validation_reminder.pending_tier_review_email_template')
+        for review in tier_review_ids:
+            if review.todo_by and review.reviewer_ids:
+                review_user_id = review.reviewer_ids.filtered(
+                    lambda review_user: review_user.name == review.todo_by
+                )
+                todo_data = {
+                    'todo_by_email': review_user_id.email,
+                    'todo_by_name': review_user_id.name
+                }
+                self.env['mail.template'].browse(template.id).with_context(todo_data).send_mail(review.id)
+
+    def get_base_url(self):
+        self.ensure_one()
+        return self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+
+
