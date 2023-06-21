5f6e4d948660179c471940c812aedd8cc42bb84c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-06-21 15:03:44 +0200

    Update __manifest__.py
    adding proper manifest

--- account_journal_upload/__manifest__.py
+++ account_journal_upload/__manifest__.py
@@ -1,14 +1,50 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2023- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name' : 'Journal Upload Button',
-    'version' : '1.1',
-    'summary': 'Journal Upload Button',
-    'sequence': 10,
-    'description': """Added Journal Upload Button""",
-    'category': 'Accounting/Accounting',
+    'name': 'Account: Journal Upload Button',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Added Journal Upload Button.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'description': """
+    Added Journal Upload Button.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-account/account_journal_upload',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    # Any module necessary for this one to work correctly
+
     'depends' : ['account'],
     'data': [
         'views/account_move_views.xml',
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
