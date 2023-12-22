bc08ecf35a9b7be5eabc2ac8f3ed339a8eacd961
Hassan <hassan@lisa.vertel.se>
2023-12-22 14:21:00 +0000

    Renamed and removed files also removed comments

--- account_enablebanking/models/__init__.py
+++ account_enablebanking/models/__init__.py
@@ -1,6 +1,4 @@
 from . import account_journal
 from . import res_config_settings
-#from . import res_company
 from . import res_bank
-from . import res_enablebanking_config
-# from . import res_openbankintegration
+from . import res_partner
--- account_enablebanking/models/res_openbankintegration.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
-
-
-class ResBank(models.Model):
-    _inherit = "res.bank"
-
-    # openbanking_integration_id = fields.One2many('res.partner')
