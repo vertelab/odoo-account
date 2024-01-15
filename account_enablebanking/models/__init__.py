c70561ac500547940c411cd31be7e674014d8aa0
Marcus Jakobsson <jakmar@darthvader.vertel.se>
2024-01-15 08:40:15 +0100

    account_enablebanking

---
+++ account_enablebanking/models/__init__.py
@@ -0,0 +1,5 @@
+from . import account_journal
+from . import res_config_settings
+from . import res_bank
+from . import res_partner
+
---
+++ account_enablebanking/models/res_partner.py
@@ -0,0 +1,73 @@
+from odoo import models, fields, api, _
+from datetime import datetime, timezone, timedelta
+import jwt as pyjwt
+import requests
+import uuid
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ResConfigSettings(models.Model):
+    _inherit = 'res.partner'
+
+    is_enable_banking_api = fields.Boolean("Enable Enablebanking API")
+
+    enable_banking_api_url = fields.Char("API URL")
+
+    enable_banking_application_id = fields.Char("Application ID")
+
+    enable_banking_redirect_url = fields.Char("Redirect URL")
+
+    enable_banking_private_key = fields.Text("Private Key")
+
+    def request_essentials(self):
+        api_url = self.enable_banking_api_url
+        private_key = self.enable_banking_private_key
+        application_id = self.enable_banking_application_id
+
+        iat = int(datetime.now().timestamp())
+        jwt_body = {
+            "iss": "enablebanking.com",
+            "aud": "api.enablebanking.com",
+            "iat": iat,
+            "exp": iat + 3600,
+        }
+        jwt = pyjwt.encode(
+            jwt_body,
+            private_key,
+            algorithm="RS256",
+            headers={"kid": application_id}, )
+        try:
+            base_headers = {"Authorization": f"Bearer {jwt.decode('utf-8')}"}
+        except AttributeError:
+            base_headers = {"Authorization": f"Bearer {jwt}"}
+        return api_url, private_key, application_id, base_headers
+
+    def action_sync_transactions_with_enable_banking(self, bank_id):
+        api_url, private_key, application_id, base_headers = self.request_essentials()
+        auth_url = self.auth_request(bank_id, api_url=api_url, base_headers=base_headers)
+        return auth_url
+
+    def _request_application_details(self, api_url, base_headers):
+        # Requesting application details
+        if not base_headers:
+            api_url, private_key, application_id, base_headers = self.request_essentials()
+        application_resp = requests.get(f"{api_url}/application", headers=base_headers).json()
+        return application_resp
+
+    def auth_request(self, bank_id=None, app=None, api_url=None, base_headers=None):
+        if not app:
+            app = self._request_application_details(api_url, base_headers)
+        body = {
+            "access": {
+                "valid_until": (datetime.now(timezone.utc) + timedelta(days=10)).isoformat()
+            },
+            "aspsp": {"name": bank_id.name, "country": bank_id.country.code},
+            "state": str(uuid.uuid4()),
+            "redirect_url": app.get("redirect_urls")[0],
+            "psu_type": "business",
+            # "psu_type": "personal",
+        }
+        auth_resp = requests.post(f"{api_url}/auth", json=body, headers=base_headers).json()
+        return auth_resp
