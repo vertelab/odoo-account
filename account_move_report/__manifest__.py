93ed5092d48719491361999a6ab5a367da5d1730
Anders Wallenquist <aw@odooutv14.vertel.se>
2024-01-21 10:18:03 +0100

    account_move_report: new module for printing Moves

---
+++ account_move_report/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Report",
+    "summary": "Print Account Moves",
+    "version": "14.0.1.0.0",
+    "category": "Account",
+    "website": "https://vertel.se/apps/odoo-account/account_move_report",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["report/account_move_report.xml"],
+    "installable": True,
+}
