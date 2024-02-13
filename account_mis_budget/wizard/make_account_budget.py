17e4d8e26f9ca5ccc6f69add9c8acc523bd3fb2f
Anders Wallenquist <anders.wallenquist@vertel.se>
2024-02-13 10:44:54 +0100

    account_mis_budget; added initial budget

--- account_mis_budget/wizard/make_account_budget.py
+++ account_mis_budget/wizard/make_account_budget.py
@@ -10,18 +10,14 @@ class MisBudgetAccount(models.Model):
     _inherit = "mis.budget.by.account"
 
     def action_create_income_budget(self):
-        
         dr = self.env['date.range.type'].search([('name','like','%mon%')])
         if len(dr) == 0:
             dr = self.env['date.range.type'].search([])
-       
         wizard_id = self.env['make.account.budget.wizard'].create({
             'budget_id': self.id,
             'date_type': dr[0].id,
             })
         return self.env.ref('account_mis_budget.action_make_account_budget').with_context({'res_id': wizard_id.id}).read()[0]
-       
-    
 
 class AccountClass(models.Model):
     _name = "custom.account.class"
@@ -56,53 +52,46 @@ class make_account_budget(models.TransientModel):
     account_ids = fields.Many2many(comodel_name="account.account",string="Accounts",required=True)
     budget_id = fields.Many2one(comodel_name="mis.budget.by.account", default=lambda b: b.env.context.get('active_id'))
 
-    use_last_years_budget = fields.Boolean(string="Use accounts from last year?", help="If checked in we create lines only for accounts that were used in an account.move.line last year")
+    use_account_from_year = fields.Boolean(string="Use accounts from last year?", help="If checked in we create lines only for accounts that were used in an account.move.line last year")
     
-    #account_class = fields.Selection([('intäkter', 'Intäkter'), ('material och varor', 'Material och varor'),('övriga kostnader', 'Övriga konstnader'), ('personalkostnader', 'Personalkostnader'), ('finansiella intäkter/kostnader','Finansiella intäkter/kostnader')], string="Account Class")
     account_class_ids = fields.Many2many(comodel_name='custom.account.class', string='Account class')
-
+    percentage_factor = fields.Float(string='Percentage Factor')
+    
     def make_report(self):
-        
-        budget_account_id = self.env['mis.budget.by.account'].browse(self._context.get('active_id'))
-
         for account in self.account_ids:
             for date_range in self.date_type.date_range_ids:
             #    env['mis.budget.kpi.item'].create({
-                if date_range.date_start > budget_account_id.date_to:
+                if date_range.date_start > self.budget_id.date_to:
                     break
-                if not self.env['mis.budget.by.account.item'].search([('budget_id','=',budget_account_id.id),
+                budget_item = self.env['mis.budget.by.account.item'].search([('budget_id','=',self.budget_id.id),
                                                        ('date_range_id','=',date_range.id),
                                                        ('date_from','=',date_range.date_start),
                                                        ('date_to','=',date_range.date_end),
-                                                       ('account_id','=',account.id)]):
-
-                    if self.env['account.move.line'].search([('account_id','=',account.id), 
-                                                             ('date', '>=', datetime(year=date_range.date_end.year, month=1, day=1) - relativedelta.relativedelta(years=1)),
-                                                             ('date', '<=', datetime(year=date_range.date_end.year, month=12, day=31) - relativedelta.relativedelta(years=1))]) and self.use_last_years_budget:
-                        self.env['mis.budget.by.account.item'].create({
-                            'budget_id': budget_account_id.id, 
+                                                       ('account_id','=',account.id)])
+                if not budget_item:
+                    budget_item = self.env['mis.budget.by.account.item'].create({
+                            'budget_id': self.budget_id.id, 
                             'date_range_id': date_range.id, 
                             'date_from': date_range.date_start, 
                             'date_to': date_range.date_end, 
                             'account_id': account.id,
                         })
-                    elif not self.use_last_years_budget:
-                        self.env['mis.budget.by.account.item'].create({
-                            'budget_id': budget_account_id.id, 
-                            'date_range_id': date_range.id, 
-                            'date_from': date_range.date_start, 
-                            'date_to': date_range.date_end, 
-                            'account_id': account.id,
-                        })
+                    if self.use_account_from_year:
+                        balance = sum(self.env['account.move.line'].search([
+                            ('account_id','=',account.id),('move_id.state','=','posted'),
+                            ('date','>=',date_range.date_start - relativedelta.relativedelta(years=1)),
+                            ('date','<=',date_range.date_end - relativedelta.relativedelta(years=1))]).mapped('balance'))
+                        logging.warning(balance)
+                        if balance > 0:
+                            budget_item.write({'debit':abs(balance) * self.percentage_factor})
+                        else:
+                            budget_item.write({'credit':abs(balance) * self.percentage_factor})
 
-    @api.onchange('budget_id','account_class_ids')
+    @api.onchange('budget_id','account_class_ids','use_account_from_year')
     def _onchange_budget_account(self):
         _logger.warning(f"{self.env.context=}")
         self.budget_id = self.env['mis.budget.by.account'].browse(self.env.context.get('active_id'))
     
     @api.onchange('account_class_ids')
     def _onchange_account_class(self):
-        ids = []
-        for account_id in self.account_class_ids.get_account_class(self.use_last_years_budget):
-            ids.append(account_id)    
-        self.write({'account_ids': [(6, 0, ids)]})
+        self.write({'account_ids': [(6, 0, self.account_class_ids.get_account_class(self.use_account_from_year))]})
