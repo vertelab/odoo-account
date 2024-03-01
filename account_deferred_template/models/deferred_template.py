6337b6034c6097be0ac67d246c0f2d929e007c83
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-03-01 15:52:14 +0100

    Update deferred_template.py

--- account_deferred_template/models/deferred_template.py
+++ account_deferred_template/models/deferred_template.py
@@ -1,1 +1,11 @@
-## adding code ....
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+class DeferrerTemplatel(models.Model):
+    _inherit = "account.move.form"
+
+    deferrer_template_ids = fields.Many2one("account.move.form", string="Select template for deferred", help="Makes accounting more simple when using deferred template.")
+  
