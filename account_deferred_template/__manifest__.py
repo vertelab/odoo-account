16a2f8f57ef2660d3c99e0a02d6aea55bbc46280
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-03-01 13:41:26 +0100

    Update __manifest__.py

--- account_deferred_template/__manifest__.py
+++ account_deferred_template/__manifest__.py
@@ -1,1 +1,46 @@
-# account_deferred_template
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account: Deferred Template',
+    'version': '16.0.0.1.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds a field for a deffered template in every product.',
+    'category': 'Accounting',
+    'description': """
+    Adds a field for a deffered template in every product.
+    This module makes it easier to set up accounting and to use deferred settings.
+    """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-account/account_deferred_template',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'depends': ['account'],
+    'data': [
+        # 'data/data.xml'
+    ],
+    'installable': 'True',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
