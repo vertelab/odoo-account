dc6d883e69161afc2771107438e7ec3ba673a676
iammiracle <miracleaayodele@gmail.com>
2024-03-08 09:12:55 +0100

    remove loggers

--- account_analytic_default_all_move_lines/models/account_move.py
+++ account_analytic_default_all_move_lines/models/account_move.py
@@ -5,38 +5,37 @@ from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
-    
-    @api.depends('tax_repartition_line_id.invoice_tax_id', 'tax_repartition_line_id.refund_tax_id') #Otherwise the tax lines wont have analytic accounts applied on itself
+
+    @api.depends('tax_repartition_line_id.invoice_tax_id',
+                 'tax_repartition_line_id.refund_tax_id')  # Otherwise the tax lines wont have analytic accounts applied on itself
     def _compute_tax_line_id(self):
-            res = super(AccountMoveLine, self)._compute_tax_line_id()
-            self._compute_analytic_account_id()
-            return res
-            
+        res = super(AccountMoveLine, self)._compute_tax_line_id()
+        self._compute_analytic_account_id()
+        return res
+
     # ~ @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id_background_lines(self):
         for record in self:
-                rec = self.env['account.analytic.default'].account_get(
-                    product_id=record.product_id.id,
-                    partner_id=record.partner_id.commercial_partner_id.id or record.move_id.partner_id.commercial_partner_id.id,
-                    account_id=record.account_id.id,
-                    user_id=record.env.uid,
-                    date=record.date,
-                    company_id=record.move_id.company_id.id
-                )
-                if rec:
-                    record.analytic_account_id = rec.analytic_id
+            rec = self.env['account.analytic.default'].account_get(
+                product_id=record.product_id.id,
+                partner_id=record.partner_id.commercial_partner_id.id or record.move_id.partner_id.commercial_partner_id.id,
+                account_id=record.account_id.id,
+                user_id=record.env.uid,
+                date=record.date,
+                company_id=record.move_id.company_id.id
+            )
+            if rec:
+                record.analytic_account_id = rec.analytic_id
 
-    
     @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id(self):
         res = super(AccountMoveLine, self)._compute_analytic_account_id()
-        
-        _logger.warning("_compute_analytic_account")
-        _logger.warning("_compute_analytic_account")
         for record in self:
             if record.exclude_from_invoice_tab or record.move_id.is_invoice(include_receipts=True):
                 rec = self.env['account.analytic.default'].account_get(
@@ -47,13 +46,9 @@ class AccountMoveLine(models.Model):
                     date=record.date,
                     company_id=record.move_id.company_id.id
                 )
-                _logger.warning(f"analytictest{record=}")
-                _logger.warning(f"analytictest{rec=}")
                 if rec:
                     record.analytic_account_id = rec.analytic_id
-                    union_record_ids=rec.analytic_tag_ids | record.analytic_tag_ids
+                    union_record_ids = rec.analytic_tag_ids | record.analytic_tag_ids
                     record.analytic_tag_ids = union_record_ids
-                    
-        return res
-                    
 
+        return res
--- account_move_line_group_by/models/account_move_line.py
+++ account_move_line_group_by/models/account_move_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#from . import res_partner
+# from . import res_partner
 
 # -*- coding: utf-8 -*-
 
@@ -19,35 +19,32 @@ import json
 import re
 import warnings
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
     @api.model
     def create(self, values):
         res = super(AccountMove, self).create(values)
-        _logger.warning("ACCOUNT CREATE")
-        _logger.warning(f"{res}")
-        _logger.warning(f"{res.line_ids}")
         for record in res.line_ids:
-            _logger.warning(record)
-            _logger.warning(record.analytic_account_id)
-            _logger.warning(record.analytic_account_id.group_id)
             if record.analytic_account_id and record.analytic_account_id.group_id:
                 record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
         # here you can do accordingly
         return res
-        
+
     def set_group_by_field_on_lines(self):
         for record in self:
-           for line in record.line_ids:
-                _logger.warning(f"{line=}")
+            for line in record.line_ids:
                 if line.analytic_account_id and line.analytic_account_id.group_id:
                     line.analytic_group_use_in_filter = line.analytic_account_id.group_id.name
 
+
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
-    
+
     @api.depends('product_id', 'account_id', 'partner_id', 'date')
     def _compute_analytic_account_id(self):
         res = super(AccountMoveLine, self)._compute_analytic_account_id()
@@ -55,34 +52,28 @@ class AccountMoveLine(models.Model):
             if record.analytic_account_id and record.analytic_account_id.group_id:
                 record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
         return res
-      
+
     # ~ @api.model
     # ~ def create(self, values):
-        # ~ res = super(AccountMoveLine, self).create(values)
-        # ~ for record in self:
-            # ~ if record.analytic_account_id and record.analytic_account_id.group_id:
-                # ~ record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
-        # ~ # here you can do accordingly
-        # ~ return res
+    # ~ res = super(AccountMoveLine, self).create(values)
+    # ~ for record in self:
+    # ~ if record.analytic_account_id and record.analytic_account_id.group_id:
+    # ~ record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
+    # ~ # here you can do accordingly
+    # ~ return res
 
-    @api.depends("analytic_account_id","analytic_account_id.group_id","analytic_account_id.group_id.name")
+    @api.depends("analytic_account_id", "analytic_account_id.group_id", "analytic_account_id.group_id.name")
     def set_analytic_group_use_in_filter_depends(self):
         for record in self:
             if record.analytic_account_id and record.analytic_account_id.group_id:
                 record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
-    
-    
-    
+
     def _set_on_analytic_group_use_in_filter_all_record(self):
         records = self.env['account.move.line'].search([])
         records.set_analytic_group_use_in_filter()
-        
-    
-    # ~ @api.model
+
     def set_analytic_group_use_in_filter(self):
         for record in self:
-            _logger.warning(f"{record}")
-            _logger.warning("record !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! _set_analytic_group_use_in_filter_default")
             if record.analytic_account_id and record.analytic_account_id.group_id:
                 record.analytic_group_use_in_filter = record.analytic_account_id.group_id.name
             else:
@@ -90,45 +81,41 @@ class AccountMoveLine(models.Model):
 
     analytic_group_use_in_filter = fields.Char()
     # ~ new_field = fields.Char(compute=_compute_analytic_group_use_in_filter_default)
-    
-    
-    
-    
+
+
 class AccountAnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
     def write(self, values):
         res = super(AccountAnalyticAccount, self).write(values)
         for record in self:
-            move_line_records = self.env['account.move.line'].search([('analytic_account_id','=',record.id)])
+            move_line_records = self.env['account.move.line'].search([('analytic_account_id', '=', record.id)])
             move_line_records.set_analytic_group_use_in_filter()
         # here you can do accordingly
         return res
-        
+
     def name_get(self):
         res = []
         for analytic in self:
             name = analytic.name
             if analytic.code:
-                name =  name + ' [' + analytic.code + '] ' 
+                name = name + ' [' + analytic.code + '] '
             if analytic.partner_id.commercial_partner_id.name:
                 name = name + ' - ' + analytic.partner_id.commercial_partner_id.name
             res.append((analytic.id, name))
         return res
-    
-    
-    
-    
+
+
 class AccountAnalyticGroup(models.Model):
     _inherit = "account.analytic.group"
 
     def write(self, values):
-        _logger.warning("account.analytic.group write !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         res = super(AccountAnalyticGroup, self).write(values)
         for record in self:
-            analytic_account_ids = self.env['account.analytic.account'].search([('group_id','=',record.id)])
+            analytic_account_ids = self.env['account.analytic.account'].search([('group_id', '=', record.id)])
             for analytic_account_id in analytic_account_ids:
-                move_line_records = self.env['account.move.line'].search([('analytic_account_id','=',analytic_account_id.id)])
+                move_line_records = self.env['account.move.line'].search(
+                    [('analytic_account_id', '=', analytic_account_id.id)])
                 move_line_records.set_analytic_group_use_in_filter()
         # here you can do accordingly
         return res
