0adc6229bc31b653e7704dd77920f1ab821709ce
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2024-03-12 10:48:06 +0100

    Update account_invoice.py

--- account_inexchange/models/account_invoice.py
+++ account_inexchange/models/account_invoice.py
@@ -43,7 +43,7 @@ class account_invoice(models.Model):
         url = settings.get_url(endpoint='documents')
         client_token = settings.inexchange_request_client_token()
         self.inexchange_file_count += 1
-        filename = (f"{self.ref.replace('/', '')}-{self.number}-{self.inexchange_file_count}")
+        filename = (f"{self.ref.replace('/', '')}-{self.name}-{self.inexchange_file_count}")
         header = {
             'ClientToken': client_token,
             'ContentDisposition': f'attachement; filename={filename}.xml',
