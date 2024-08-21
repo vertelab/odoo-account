65f65c98c604ffbc32c1254708a0923fefd4a47e
iammiracle <miracleaayodele@gmail.com>
2024-08-21 13:50:30 +0200

    create bulk invoices

---
+++ account_bulk_invoices/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2023- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account: Bulk Invoices',
+    'version': '17.0.0.0.0',
+    'summary': 'A wizard to create bulk invoices',
+    'category': 'Accounting',
+    'description': """
+        A wizard to create bulk invoices
+    """,
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-account/account_bulk_invoices',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'depends': ['account'],
+    'data': [
+        'views/bulk_account_view.xml',
+    ],
+    'sequence': 5,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ account_bulk_invoices/models/account_move.py
@@ -0,0 +1,34 @@
+from odoo import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    partner_ids = fields.Many2many("res.partner", string="Customers")
+
+    def invoice_vals(self):
+        vals = {
+            'move_type': 'out_invoice',
+            'invoice_line_ids': self.invoice_line_ids.ids,
+            'journal_id': self.journal_id.id,
+            'ref': self.ref,
+            'date': self.date,
+            'fiscal_position_id': self.fiscal_position_id.id,
+            'company_id': self.company_id.id,
+            'invoice_user_id': self.invoice_user_id.id,
+            'auto_post': self.auto_post,
+            'to_check': self.to_check,
+            'invoice_date_due': self.invoice_date_due,
+            'payment_reference': self.payment_reference,
+            'invoice_payment_term_id': self.invoice_payment_term_id.id,
+        }
+        return vals
+
+    def action_create_invoice(self):
+        vals = self.invoice_vals()
+        move_id = self.env['account.move'].create(vals)
+
+        for partner_id in self.partner_ids:
+            move_id.copy({'partner_id': partner_id.id})
+        self.unlink()
+        move_id.unlink()
