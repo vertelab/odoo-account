1d18bb6bacaa3678bd0b8d322ac27bf364dbc58d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 14:26:17 +0200

    Update __manifest__.py

--- account_admin_rights/__manifest__.py
+++ account_admin_rights/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Account: Admin Rights',
-    'version': '16.0.0.1.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Account Admin Rights',
     'category': 'Accounting',
     'description': """
