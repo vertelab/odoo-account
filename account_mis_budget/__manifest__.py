6e6fb5977cf6d9801cc1254ad5aa69f5176e04ba
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:32:50 +0200

    Update __manifest__.py

--- account_mis_budget/__manifest__.py
+++ account_mis_budget/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) {year} {company} (<{mail}>)
-#    All Rights Reserved
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,15 +15,15 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 #
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'Account MIS Budget',
-    'version': '16.0.0.0.0',
+    'name': 'Account: MIS Budget',
+    'version': '0.1',
     'summary': """
         Lets us generate budget based on the previous year.
         Also lets us add two budgets together into a new one.
@@ -34,8 +34,10 @@
         Long description of module's purpose
     """,
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-',
+    'website': 'https://vertel.se/apps/odoo-account/account_mis_budget',
     'license': 'AGPL-3',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
     'depends': ['date_range', 'account', 'mis_builder_budget', 'mis_builder'],
     'data': [
         "security/ir.model.access.csv",
