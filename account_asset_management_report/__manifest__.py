4500793c09e05abde47812ece74418c207d1bfbe
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:27:04 +0200

    Update __manifest__.py

--- account_asset_management_report/__manifest__.py
+++ account_asset_management_report/__manifest__.py
@@ -20,14 +20,14 @@
 ##############################################################################
 
 {
-    'name': 'Account Asset Management Report',
-    'version': '16.0.0.0.2',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'name': 'Account: Asset Management Report',
+    'version': '0.2',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Account Asset Management Report (Pivot view).',
     'category': 'Accounting',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-account/account_asset_management_component',
+    'website': 'https://vertel.se/apps/odoo-account/account_asset_management_report',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
