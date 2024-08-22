187fef61ff98a3c3ece11ff5274359c6a2907cf7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:35:42 +0200

    Update __manifest__.py

--- account_mis_cash_flow_extended/__manifest__.py
+++ account_mis_cash_flow_extended/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) {year} {company} (<{mail}>)
-#    All Rights Reserved
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,27 +15,29 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 #
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': '',
-    'version': '16.0.0.0.0',
+    'name': 'Account: MIS cash flow extend',
+    'version': '0.1',
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.odoo.com""",
+
+    """,
     'category': 'Accounting', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
     'description': """
-        Long description of module's purpose
+    
     """,
     'sequence': 1,
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-',
+    'website': 'https://vertel.se/apps/odoo-account/account_mis_cash_flow_extended',
     'images': ['static/description/banner.png'], # 560x280
     'license': 'AGPL-3',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
     'depends': ['account_mis_budget', 'mis_builder','account','mis_builder_budget', 'mis_builder_cash_flow'],
     'data': [
         'security/ir.model.access.csv',
