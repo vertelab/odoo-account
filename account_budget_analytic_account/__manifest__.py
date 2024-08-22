084b911949c238bd5d351913ab0bf73650de4d83
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:27:49 +0200

    Update __manifest__.py

--- account_budget_analytic_account/__manifest__.py
+++ account_budget_analytic_account/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,9 +20,9 @@
 ##############################################################################
 
 {
-    'name': 'MIS Builder for Analytic Account',
-    'version': '16.0.0.0.2',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'name': 'Account: MIS Builder for Analytic Account',
+    'version': '0.2',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'MIS Builder for Analytic Account.',
     'category': 'Accounting',
     #'sequence': '1'
