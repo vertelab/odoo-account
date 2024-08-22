c81b674bbcfcab112b99286b1932a720acd010af
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:28:22 +0200

    Update __manifest__.py

--- account_deferred_revenue_expenses/__manifest__.py
+++ account_deferred_revenue_expenses/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,9 +20,9 @@
 ##############################################################################
 
 {
-    'name': 'Account Deferred Revenue Expenses',
-    'version': '16.0.0.1.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'name': 'Account: Deferred Revenue Expenses',
+    'version': '1.0',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Account Admin Rights',
     'category': 'Accounting',
     'description': """
