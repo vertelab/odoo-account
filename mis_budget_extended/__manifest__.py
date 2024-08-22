666abf02c0fe0293cd6a10b8f4f24744478cfd1b
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 14:18:08 +0200

    Update __manifest__.py

--- mis_budget_extended/__manifest__.py
+++ mis_budget_extended/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,7 +21,7 @@
 
 {
     'name': 'MIS Budget Extended (To be Deprecated)',
-    'version': '16.0.0.0.2',
+    'version': '0.2',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'MIS Budget Extended.',
     'category': 'Accounting',
