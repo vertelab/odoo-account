dc69e52c93bba18c4d885ce7b9d078b71ce00eb5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:19:23 +0200

    Update __manifest__.py

--- account_accountant_ce/__manifest__.py
+++ account_accountant_ce/__manifest__.py
@@ -20,9 +20,9 @@
 ##############################################################################
 
 {
-    'name': 'Bokföring (CE)',
-    'version': '16.0.0.0.0',
-    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'name': 'Account: Bokföring (CE)',
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'License Manager.',
     'category': 'Accounting',
     'description': """
