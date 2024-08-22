5c10c07f8cd32e076c17467f92e88e54b22e403f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-22 13:25:51 +0200

    Update __manifest__.py

--- account_asset_lot_stock_personal_equipment/__manifest__.py
+++ account_asset_lot_stock_personal_equipment/__manifest__.py
@@ -20,14 +20,14 @@
 ##############################################################################
 
 {
-    'name': 'Account Asset Stock Lot Equipment',
-    'version': '16.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'name': 'Account: Asset Stock Lot Equipment',
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Connect Created Asset to equipment.',
     'category': 'Accounting',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-account/account_asset_lot_stock',
+    'website': 'https://vertel.se/apps/odoo-account/account_asset_lot_stock_personal_equipment',
     'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
