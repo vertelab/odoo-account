0ce9c088a3632da2b4a77464262b688c34a818b2
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2024-09-04 16:55:01 +0200

    Update res_partner.py

--- account_fortnox/models/res_partner.py
+++ account_fortnox/models/res_partner.py
@@ -80,8 +80,8 @@ class Partner(models.Model):
                         "Customer": {
                             "Address1": partner.street,
                             "City": partner.city,
-                            "CountryCode": "SE",
-                            "Currency": "SEK",
+                            "CountryCode": partner.country_id.code,
+                            #"Currency": "SEK",
                             "Email": partner.email or None,
                             "Name": partner.commercial_partner_id.name,
                             "Phone1": partner.commercial_partner_id.phone,
@@ -112,8 +112,8 @@ class Partner(models.Model):
                         "Customer": {
                             "Address1": partner.street,
                             "City": partner.city,
-                            "CountryCode": "SE",
-                            "Currency": "SEK",
+                            "CountryCode": partner.country_id.code,
+                            #"Currency": "SEK",
                             "Email": partner.email or None,
                             "Name": partner.commercial_partner_id.name,
                             "Phone1": partner.commercial_partner_id.phone,
