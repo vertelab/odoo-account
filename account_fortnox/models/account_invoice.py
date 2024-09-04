891c89b12edafde67dc0a4d25205f455ebbe9bd8
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2024-09-04 15:43:27 +0200

    Update account_invoice.py

--- account_fortnox/models/account_invoice.py
+++ account_fortnox/models/account_invoice.py
@@ -213,7 +213,7 @@ class AccountInvoice(models.Model):
     def fortnox_invoice_vals(self, invoice, invoice_lines):
         invoice_vals = {
             "Comments": "",
-            "Currency": "SEK",
+            "Currency": invoice.currency_id.name,
             "CustomerName": invoice.partner_id.commercial_partner_id.name,
             "CustomerNumber": invoice.partner_id.commercial_partner_id.fortnox_ref,
             "DueDate": invoice.invoice_date_due.strftime('%Y-%m-%d'),
