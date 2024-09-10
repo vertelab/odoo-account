5403442e99ad83b5d28ece961b47455d7724cda3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-09-10 09:01:24 +0200

    Update __manifest__.py

--- account_asset_lot_stock/__manifest__.py
+++ account_asset_lot_stock/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Account Asset Stock Lot',
+    'name': 'Account: Asset Stock Lot',
     'version': '17.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Create an asset connected to a Lot number.',
