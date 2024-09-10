3ed9847abf57a4e689f6446a138c47e23df19572
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-09-10 09:54:56 +0200

    Update __manifest__.py

--- account_fortnox_display_name/__manifest__.py
+++ account_fortnox_display_name/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) {year} {company} (<{mail}>)
+#    Copyright (C) 2024- Vertel AB  info@vertel.se
 #    All Rights Reserved
 #
 #    This program is free software: you can redistribute it and/or modify
