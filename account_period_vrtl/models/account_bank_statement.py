f94130748cb13e2cc551c7f76c04ed9eb11dbdac
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2024-12-18 11:26:39 +0100

    Update account_bank_statement.py

--- account_period_vrtl/models/account_bank_statement.py
+++ account_period_vrtl/models/account_bank_statement.py
@@ -33,20 +33,17 @@ class AccountBankStatementLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-			if 'date' in vals:
-				date = vals['date']
-					_logger.warning(f"{date=}")
-					try:
-						date = datetime.strptime(date, "%Y-%m-%d")
-					except TypeError:
-						pass
-					period = vals['period_id'] = self.env['account.period'].date2period(date).id
-					if not period:
-						date_formated = datetime.strftime(date, "%Y-%m-%d")
-						raise UserError(_(f"There is no period for the date {date_formated}, please choose another date or "
-										  f"create a period for that date."))
-					_logger.warning(f"{period=}")
+                        if 'date' in vals:
+                                date = vals['date']
+                                        _logger.warning(f"{date=}")
+                                        try:
+                                                date = datetime.strptime(date, "%Y-%m-%d")
+                                        except TypeError:
+                                                pass
+                                        period = vals['period_id'] = self.env['account.period'].date2period(date).id
+                                        if not period:
+                                                date_formated = datetime.strftime(date, "%Y-%m-%d")
+                                                raise UserError(_(f"There is no period for the date {date_formated}, please choose another date or "
+                                                                                  f"create a period for that date."))
+                                        _logger.warning(f"{period=}")
         return super(AccountBankStatementLine, self).create(vals_list)
-    
-    
-    
