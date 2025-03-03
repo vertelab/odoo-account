ec3652886b2874e9e432078d3d8d22b96126c169
iammiracle <miracleaayodele@gmail.com>
2025-03-03 14:35:47 +0100

    moved code from skf_ai_agent to invoice and did cleanup

--- account_invoice_ai/models/ai_quest.py
+++ account_invoice_ai/models/ai_quest.py
@@ -97,24 +97,6 @@ class AIQuest(models.Model):
             currency_id = self.env['res.currency'].search([('symbol', '=', currency)], limit=1)
         return currency_id.id
 
-    # def _create_vendor_bill(self, res, session):
-    #     invoice_data = self.parse_invoice_data(res)
-    #     customer_name = invoice_data.pop('customer', False)
-    #     vendor_in_eu = invoice_data.pop('vendor_in_eu', False)
-    #     customer_in_eu = invoice_data.pop('customer_in_eu', False)
-    #     period_id = self.env['account.period'].date2period(invoice_data.get('date', fields.Date.today())).id
-    #     invoice_data['partner_id'] = self._get_or_create_partner(invoice_data.pop('vendor', False))
-    #     invoice_data['currency_id'] = self._get_currency(invoice_data.pop('currency', False))
-    #     invoice_data['period_id'] = period_id
-    #     invoice_data['invoice_date'] = invoice_data.get('date')
-    #     invoice_data['move_type'] = 'in_invoice'
-    #     invoice_data['invoice_line_ids'] = self._invoice_lines(invoice_data.pop('invoice_line_ids'))
-    #
-    #     if session.move_id:
-    #         session.move_id.write(invoice_data)
-    #     else:
-    #         self.env['account.move'].create(invoice_data)
-
     def _create_vendor_bill(self, invoice_data, session, partner_id):
         if session.ai_quest_id.company_id:
             self = self.with_context(company_id=session.ai_quest_id.company_id.id)
