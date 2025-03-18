39fed43e182a6bb543ce076779e9724b6d3651b9
Jakob Krabbe <jakob@syscare.se>
2025-03-18 14:50:07 +0000

    odoo-account: fixing extra space. T/0875

--- account_asset_lot_stock/models/stock.py
+++ account_asset_lot_stock/models/stock.py
@@ -181,7 +181,7 @@ class StockPicking(models.Model):
                        if not lot_id.asset_id and (move.asset_profile_id or lot_id.asset_profile_id):
                             vals = lot_id._prepare_asset_vals(stock_picking, move)
                             lot_id.create_asset(vals)
-                        elif lot_id.asset_id and lot_id.asset_id.state == "draft":
+                       elif lot_id.asset_id and lot_id.asset_id.state == "draft":
                             lot_id.update_partner_asset(stock_picking.partner_id)
 
                             #Change partner 
