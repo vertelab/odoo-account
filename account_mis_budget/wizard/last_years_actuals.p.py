1df9a8c65768039c4677030ec1e2e97f179a9c17
vertelbot <vertelbot@vertel.se>
2025-04-08 14:34:13 +0000

    odoobranchpfile odoo-account from 16.0. Do not p-file sync

---
+++ account_mis_budget/wizard/last_years_actuals.p.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+import logging
+import dateutil.relativedelta as relativedelta
+
+class LastYearsActuals(models.TransientModel):
+    _name = "last.years.actuals"
+    _description = "Retrieving the past budgets"
+    
+    #date_type = fields.Many2one('date.range.type', string='Date type', required=True) # Month, Weeks, Every two weeks
+    percentage_factor = fields.Float(string="Percentage Factor")
+
+    def modify_budget(self):
+        active_ids = self._context.get('active_ids', [])
+        logging.warning(f"{self._context=}")
+
+        # Log the value of active_ids to debug
+        logging.warning(f'active_ids: {active_ids}')
+
+        # Use the active_ids to retrieve the selected records
+        selected_records = self.env['mis.budget.by.account.item'].browse(active_ids)
+
+         #Log the selected records
+        for record in selected_records:
+            lines = self.env['account.move.line'].search([('account_id','=',record.account_id.id),('move_id.state','=','posted'),('date','>=',record.date_from - relativedelta.relativedelta(years=1)),('date','<=',record.date_to - relativedelta.relativedelta(years=1))])
+
+            balance = sum(lines.mapped('balance'))
+            logging.warning(balance)
+
+            if balance > 0:
+                record.write({'debit':abs(balance) * self.percentage_factor})
+            else:
+                record.write({'credit':abs(balance) * self.percentage_factor})
