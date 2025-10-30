dd272e35dc2456dce0b622f97221649b54bbc768
marcuszxc <marcus.karlsson@vertel.se>
2025-10-30 16:01:07 +0000

    account_enablebanking: Ported from 16 to 18

---
+++ account_enablebanking/models/res_config_settings.py
@@ -0,0 +1,22 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    interval_number = fields.Integer(string="Interval Number", config_parameter='enablebanking.interval_number')
+
+    interval_type = fields.Selection([
+        ('minutes', 'Minutes'),
+        ('hours', 'Hours'),
+        ('days', 'Days'),
+        ('weeks', 'Weeks'),
+        ('months', 'Months')], string="Interval Type", config_parameter='enablebanking.interval_type')
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env.ref('account_enablebanking.enable_banking_transaction_sync').write({
+            'interval_number': self.interval_number,
+            'interval_type': self.interval_type
+        })
