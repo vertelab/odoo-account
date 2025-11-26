8ff9b1b945327bf1efa185fab0d21c90eff994f8
marcuszxc <marcus.karlsson@vertel.se>
2025-11-26 13:02:33 +0000

    account_payment_order_hr: Rewriten

--- account_payment_order_hr/models/payment_order.py
+++ account_payment_order_hr/models/payment_order.py
@@ -8,12 +8,18 @@ _logger = logging.getLogger(__name__)
 class AccountPaymentOrder(models.Model):
     _inherit = 'account.payment.order'
 
-    user_id = fields.Many2one(comodel_name="res.users")
-    user_boss_id = fields.Many2one(comodel_name="res.users")
+    user_id = fields.Many2one(comodel_name="res.users",string="responsible")
+    user_boss_id = fields.Many2one(comodel_name="res.users", string="responsible boss",compute="_compute_user_boss_id", store=True)
 
-    @api.onchange("user_id")
-    def set_boss(self):
-        if self.user_id and self.user_id.employee_parent_id and self.user_id.employee_parent_id.user_id:
-            self.user_boss_id = self.user_id.employee_parent_id.user_id.id
+    def create(self, vals_list):
+        user_id = self.env.context.get("uid")
+        vals_list.update({"user_id":user_id})
+        return super().create(vals_list)
 
-
+    @api.depends("user_id")
+    def _compute_user_boss_id(self):
+        for rec in self:
+            if rec.user_id and rec.user_id.sudo().employee_parent_id and rec.user_id.sudo().employee_parent_id.user_id:
+                rec.user_boss_id = rec.user_id.sudo().employee_parent_id.user_id.id
+            else:
+                rec.user_boss_id = False
